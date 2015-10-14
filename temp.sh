ead4c47f81c8afc98ff4b42729700f16610ae2e1
Ross <ross@treasurylane.com>
2015-10-14 15:19:42 -0400

    updating temp

--- temp.sh
+++ temp.sh
@@ -1,3 +1,1 @@
- ansible-playbook -vvvv --inventory-file=ansible_hosts make-sandbox.yaml
-ansible-playbook -vvvv --inventory-file=testing host-server.yaml
 ansible-playbook -vvvv --inventory-file=testing host-server.yaml
