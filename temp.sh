76c9f66d100e2875d183a659b45e232e1e46bbcd
Ross <ross@treasurylane.com>
2015-10-14 15:18:06 -0400

    moving things around for best bractices

---
+++ temp.sh
@@ -0,0 +1,3 @@
+ ansible-playbook -vvvv --inventory-file=ansible_hosts make-sandbox.yaml
+ansible-playbook -vvvv --inventory-file=testing host-server.yaml
+ansible-playbook -vvvv --inventory-file=testing host-server.yaml
