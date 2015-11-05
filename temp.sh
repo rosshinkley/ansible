a87eb9c1ba4353511f5c014b5acd03656420782c
Ross <rosshinkley@gmail.com>
2015-11-04 20:57:50 -0500

    setting up chromebook parts

--- temp.sh
+++ temp.sh
@@ -1,1 +1,5 @@
-ansible-playbook -vvvv --inventory-file=testing host-server.yaml
+#ansible-playbook -vvvv --inventory-file=testing host-server.yaml
+
+#for the chromebook!
+ansible-playbook -vvvv  --inventory-file=blag --ask-become-pass dev-box.yaml
+#ansible-playbook -vvvv --inventory-file=blag --ask-become-pass hello.yaml
