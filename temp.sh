927fab32ced4e7f46731c2ec0cc947b631d66f18
ross <rosshinkley@gmail.com>
2015-11-12 21:09:39 -0500

    adding tmux to dev

--- temp.sh
+++ temp.sh
@@ -1,5 +1,6 @@
 #ansible-playbook -vvvv --inventory-file=testing host-server.yaml
 
 #for the chromebook!
-ansible-playbook -vvvv  --inventory-file=blag --ask-become-pass dev-box.yaml
+#ansible-playbook -vvvv  --inventory-file=blag --ask-become-pass dev-box.yaml
+ansible-playbook -vvvv  --inventory-file=blag dev-box.yaml
 #ansible-playbook -vvvv --inventory-file=blag --ask-become-pass hello.yaml
