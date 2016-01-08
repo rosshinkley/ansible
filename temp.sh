fc3bac0b271181f52edc5fd09c9e8710b87aed73
Ross <rosshinkley@gmail.com>
2016-01-08 16:00:17 -0500

    tinkering with testing again

--- temp.sh
+++ temp.sh
@@ -1,6 +1,6 @@
-#ansible-playbook -vvvv --inventory-file=testing host-server.yaml
+ansible-playbook -vvvv --inventory-file=testing dev-box.yaml
 
 #for the chromebook!
 #ansible-playbook -vvvv  --inventory-file=blag --ask-become-pass dev-box.yaml
-ansible-playbook -vvvv  --inventory-file=blag dev-box.yaml
-#ansible-playbook -vvvv --inventory-file=blag --ask-become-pass hello.yaml
+#ansible-playbook -vvvv  --inventory-file=blag dev-box.yaml
+#ansible-playbook -vvvv --inventory-file=testing hello.yaml
